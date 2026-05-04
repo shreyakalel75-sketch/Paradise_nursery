6f041014fe30280a1100d2ec26220dbc41295cad
shreyakalel75-sketch <shreya.kalel75@gmail.com>
2026-05-04 14:27:50 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <p>Welcome to Paradise Nursery, where we believe every home deserves a touch of nature. Our curated collection of houseplants is designed to bring peace and fresh air into your life.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
