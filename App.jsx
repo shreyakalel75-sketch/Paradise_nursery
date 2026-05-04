d6e617faedaa0c7afb92d96cef1ba463eac932ee
shreyakalel75-sketch <shreya.kalel75@gmail.com>
2026-05-04 14:24:48 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import ProductList from './ProductList';
+import './App.css';
+import AboutUs from './AboutUs';
+
+function App() {
+  const [showProductList, setShowProductList] = useState(false);
+
+  const handleGetStartedClick = () => {
+    setShowProductList(true);
+  };
+
+  return (
+    <div className="app-container">
+      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+        <div className="background-image"></div>
+        <div className="content">
+          <h1>Paradise Nursery</h1>
+          <p>Where Greenery Meets Serenity</p>
+          <button className="get-started-button" onClick={handleGetStartedClick}>
+            Get Started
+          </button>
+          <AboutUs />
+        </div>
+      </div>
+      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+        <ProductList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
